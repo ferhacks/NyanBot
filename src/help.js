5f0cf7df0366cad277f5eb7616ef4a343c7dc762
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-11 09:40:05 -0600

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,66 +1,58 @@
 const help = (prefix) => {
-	return `
-┌────────── •✧✧• ──────────┐
+	return `┌────────── •✧✧• ──────────┐
 |✅> *𝗛𝗼𝗹𝗮, 𝘀𝗼𝘆 𝗡𝘆𝗮𝗻𝗯𝗼𝘁* <✅
 | *𝗘𝘀𝘁𝗮𝘀 𝘀𝗼𝗻 𝗮𝗹𝘂𝗻𝗮𝘀 𝗱𝗲 𝗺𝗶𝘀 𝗳𝘂𝗻𝗰𝗶𝗼𝗻𝗲𝘀:𝗗*
-|🐬>ԑ̮̑ঙ< *𝙉𝙮𝙖𝙣𝘽𝙤𝙩* >ԑ̮̑ঙ<🐬
-|
+|🐬>ԑ̮̑ঙ< *𝙉𝙮𝙖𝙣𝘽𝙤𝙩* >ԑ̮̑ঙ<🐬\n\n
 |╔═.✵.═══════════╗
-|| *.Nyan* 
+|| *${prefix}Nyan* 
 |╚═══════════.✵.═╝
 |──────────────────────────────
-| *.sticker*
+| *${prefix}sticker*
 | _Imagen/video/gif a sticker_
 |──────────────────────────────
-| *.toimg*
+| *${prefix}toimg*
 | _Sticker a imagen_
 |──────────────────────────────
-| *.gtts*
+| *${prefix}gtts*
 | _Texto a voz_
 |❌ *DEBES ESPECIFICAR EL IDIOMA* ❌
 |──────────────────────────────
-| *.loli*
+| *${prefix}loli*
 | _Imagen Anime_
 |──────────────────────────────
-| *.imagen*
+| *${prefix}imagen*
 | _Busca cualquier imagen de google_
 |──────────────────────────────
-| *.tiktokstalck*
+| *${prefix}tiktokstalck*
 | _Informacion sobre una cuenta de tiktok_
 |❌ *NESECITAS NOMBRE DE USUARIO❌
 |──────────────────────────────
-| _*𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗽𝗮𝗿𝗮 𝗔𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝗱𝗼𝗿𝗲𝘀*_
-|
-|
-| *.promote*
+| _*𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗽𝗮𝗿𝗮 𝗔𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝗱𝗼𝗿𝗲𝘀*_\n\n
+| *${prefix}promote*
 | _Otorga admin a un participante_
 |──────────────────────────────
-| *.demote*
+| *${prefix}demote*
 | _Quita admin:v_
 |──────────────────────────────
-| *.kick*
+| *${prefix}kick*
 | _Elimina participantes_
 |──────────────────────────────
-| *.add*
+| *${prefix}add*
 | _Agrega participantes_
 |──────────────────────────────
-| *.clone*
+| *${prefix}clone*
 | _Cambia la foto de perfil del bot_
 |❌PARA ESTO ETIQUETA A LA PERSONA QUE QUIERES CLONAR❌
 |──────────────────────────────
-| *.kickall*
+| *${prefix}kickall*
 | _Elimina a todos los participantes de un grupo_
-|❌ *SOLO PARA EL DUEÑO DEL GRUPO* ❌
-|
-|⚠ *USA ESTE COMANDO BAJO TU RESPONSABILIDAD!!* ⚠ 
+|❌ *SOLO PARA EL DUEÑO DEL GRUPO* ❌\n
+|⚠ *USA ESTE COMANDO BAJO TU RESPONSABILIDAD!!* ⚠\n
 └──────────────────────────────
-🔥ஜீ۞ஜீ🐬•.¸¸.•*``*•.¸¸.•🐬ஜீ۞ஜீ🔥
-*No son todos los comandos, este bot esta apenas desarrollandose, por lo tanto solo puedes hacer uso de esos comandos*
-
-_*Para alguna duda o aclaracion, comunicate con el propietario*_
-
-👑Samu330👑:
-wa.me/+529984907794
+*No son todos los comandos, este bot esta apenas desarrollandose, por lo tanto solo puedes hacer uso de esos comandos*\n\n
+_*Para alguna duda o aclaracion, comunicate con el propietario*_\n\n
+👑Samu330👑:\n
+wa.me/+529984907794\n
 ︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿
 │ │ │ │. ;; [🐬 *NyanBot* 🐬]:;
 │ │ │ ✧
