494a231a1e9a9c4fda04845f4b6a2f263bc10e69
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-11 08:55:17 -0600

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,11 +1,68 @@
 const help = (prefix) => {
-	return `> *Sticker Commands* <
-command : *${prefix}sticker* or *${prefix}stiker*
+	return `
+┌────────── •✧✧• ──────────┐
+|✅> *𝗛𝗼𝗹𝗮, 𝘀𝗼𝘆 𝗡𝘆𝗮𝗻𝗯𝗼𝘁* <✅
+| *𝗘𝘀𝘁𝗮𝘀 𝘀𝗼𝗻 𝗮𝗹𝘂𝗻𝗮𝘀 𝗱𝗲 𝗺𝗶𝘀 𝗳𝘂𝗻𝗰𝗶𝗼𝗻𝗲𝘀:𝗗*
+|🐬>ԑ̮̑ঙ< *𝙉𝙮𝙖𝙣𝘽𝙤𝙩* >ԑ̮̑ঙ<🐬
+|──────────────────────────────
+| *${prefix}sticker*
+| _Imagen/video/gif a sticker_
+|──────────────────────────────
+| *${prefix}toimg*
+| _Sticker a imagen_
+|──────────────────────────────
+| *${prefix}gtts*
+| _Texto a voz_
+|❌ *DEBES ESPECIFICAR EL IDIOMA* ❌
+|──────────────────────────────
+| *${prefix}loli*
+| _Imagen Anime_
+|──────────────────────────────
+| *${prefix}imagen*
+| _Busca cualquier imagen de google_
+|──────────────────────────────
+| *${prefix}tiktokstalck*
+| _Informacion sobre una cuenta de tiktok_
+|❌ *NESECITAS NOMBRE DE USUARIO❌
+|──────────────────────────────
+| _*𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀 𝗽𝗮𝗿𝗮 𝗔𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝗱𝗼𝗿𝗲𝘀*_
+|
+|
+| *${prefix}promote*
+| _Otorga admin a un participante_
+|──────────────────────────────
+| *${prefix}demote*
+| _Quita admin:v_
+|──────────────────────────────
+| *${prefix}kick*
+| _Elimina participantes_
+|──────────────────────────────
+| *${prefix}add*
+| _Agrega participantes_
+|──────────────────────────────
+| *${prefix}clone*
+| _Cambia la foto de perfil del bot_
+|❌PARA ESTO ETIQUETA A LA PERSONA QUE QUIERES CLONAR❌
+|──────────────────────────────
+| *${prefix}kickall*
+| _Elimina a todos los participantes de un grupo_
+|❌ *SOLO PARA EL DUEÑO DEL GRUPO* ❌
+|
+|⚠ *USA ESTE COMANDO BAJO TU RESPONSABILIDAD!!* ⚠ 
+└──────────────────────────────
+🔥ஜீ۞ஜீ🐬•.¸¸.•*`*•.•ஜீ☼۞☼ஜீ•.•*`*•.¸¸.•🐬ஜீ۞ஜீ🔥
+*No son todos los comandos, este bot esta apenas desarrollandose, por lo tanto solo puedes hacer uso de esos comandos*
 
+_*Para alguna duda o aclaracion, comunicate con el propietario*_
 
-
-
-aaaaaaaaaaaaaaaaaa`
+👑Samu330👑:
+wa.me/+529984907794
+︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿︿
+│ │ │ │. ;; [🐬 *NyanBot* 🐬]:;
+│ │ │ ✧
+│ │ ◆
+│ ✧
+◆`
 }
 
 exports.help = help
