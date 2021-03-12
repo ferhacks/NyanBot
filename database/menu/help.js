1d1b6660c8be662a31b3f80b9661b92facda745d
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-11 18:48:26 -0600

    Update help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -1,45 +1,44 @@
-const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+const help = (pushname, prefix, reqXp, uangku) => {
         return `
-┏ *〈 ${botName} 〉*
-╿
-┷┯ *〈 USER INFO 〉*
-   ╽
-   ┠≽ *Name* : ${pushname}
-   ┠≽ *XP* : ${reqXp}
-   ┠≽ *Money* : ${uangku}
-   ┠≽ *Registered : ✔️
-   ╿
+╔ *〈 🐬𝗡𝘆𝗮𝗻𝗕𝗼𝘁🐬 〉*
+║
+➥ *〈 USER INFO 〉*
+   ║
+   ║≽ *Name* : ${pushname}
+   ║≽ *XP* : ${reqXp}
+   ║≽ *Money* : ${uangku}
+   ║≽ *Registered : ✔️
+   ║
 ┯┷ *〈 BOT INFO 〉*
-╽
-┠≽ *Prefix* : 「  ${prefix}  」
-┠≽ *Creator* : ${ownerName}
-┠≽ *Version* : 0.0.5
-╿
+║
+║≽ *Prefix* : 「  ${prefix}  」
+║≽ *Creator* : _*Samu330*_
+║≽ *Version* : 0.0.2
+║
 ┷┯ *〈 MENU 〉*
-   ┠≽ *${prefix}logomakermenu*
-   ┠≽ *${prefix}imagemakermenu*
-   ┠≽ *${prefix}stickermakermenu*
-   ┠≽ *${prefix}searchmenu*
-   ┠≽ *${prefix}educationmenu*
-   ┠≽ *${prefix}kerangmenu*
-   ┠≽ *${prefix}downloadermenu*
-   ┠≽ *${prefix}mememenu*
-   ┠≽ *${prefix}groupmenu*
-   ┠≽ *${prefix}soundmenu*
-   ┠≽ *${prefix}musicmenu*
-   ┠≽ *${prefix}islammenu*
-   ┠≽ *${prefix}stalkmenu*
-   ┠≽ *${prefix}wibumenu*
-   ┠≽ *${prefix}18+menu*
-   ┠≽ *${prefix}funmenu*
-   ┠≽ *${prefix}todmenu*
-   ┠≽ *${prefix}informationmenu*
-   ┠≽ *${prefix}stayonscreenmenu*
-   ┠≽ *${prefix}xpmenu*
-   ┠≽ *${prefix}limitmenu*
-   ┠≽ *${prefix}ownermenu*
-   ┠≽ *${prefix}othermenu*
-   ╿ *${ownerName}*,
-   ╰╼≽ *Developer © ${botName}`
+   ║≽ *${prefix}1 *18+ menu*
+   ║≽ *${prefix}2 *downloader menu*
+   ║≽ *${prefix}3 *education menu*
+   ║≽ *${prefix}4 *fun menu*
+   ║≽ *${prefix}5 *group menu*
+   ║≽ *${prefix}6 *image maker menu*
+   ║≽ *${prefix}7 *information menu*
+   ║≽ *${prefix}8 *kerang menu*
+   ║≽ *${prefix}9 *logo maker menu*
+   ║≽ *${prefix}10 *meme menu*
+   ║≽ *${prefix}11 *music menu*
+   ║≽ *${prefix}12 *other menu*
+   ║≽ *${prefix}13 *owner menu*
+   ║≽ *${prefix}14 *search menu*
+   ║≽ *${prefix}15 *sound menu*
+   ║≽ *${prefix}16 *stalk menu*
+   ║≽ *${prefix}17 *stay on screen menu*
+   ║≽ *${prefix}18 *sticker maker menu*
+   ║≽ *${prefix}19 *tod menu*
+   ║≽ *${prefix}20 *wibu menu*
+   ║≽ *${prefix}21 *xp menu*
+   ║≽ *${prefix}22 *limit menu*
+   ║ *🔥𝗡𝘆𝗮𝗻𝗕𝗼𝘁🔥*,
+   ╚≽ *Developer ©Samu330`
 }
 exports.help = help
