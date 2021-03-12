73289f7b726788eb52d053b2aa746e050f5be1c6
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-11 18:54:12 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,17 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y nodejs libwebp ffmpeg wget tesseract
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm i imgbb-uploader
+npm cache clean -f
 npm install
+npm i got
 
-echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[INFO] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+echo "[INFO] If my script is error, please report a error to me."
+echo "[UPDATE] Move prefix bot to option"
