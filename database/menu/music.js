283dd4cff704e6c8bbdc885dbb5933623e9d6a2e
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-12 09:28:56 -0600

    Update music.js

--- database/menu/music.js
+++ database/menu/music.js
@@ -1,30 +1,30 @@
 const music = (prefix, botName, ownerName) => {
         return `
-┏ *〈 ${botName} 〉*
-╿
-┷┯ *〈 BOT INFO 〉*
-   ╽
-   ┠≽ *Prefix* : 「  ${prefix}  」
-   ┠≽ *Creator* : ${ownerName}
-   ┠≽ *Version* : 0.0.5
-   ╿
-┯┷ *〈 ABOUT 〉*
-╽
-┠≽ *${prefix}info* (error)
-┃ *Desc* : Show Bot Details
-┠──────────────╼
-┠≽ *${prefix}blocklist*
-┃ *Desc* : Show Blocked User
-┠──────────────╼
-┠≽ *${prefix}chatlist* (error)
-┃ *Desc* : Show All Chat User
-┠──────────────╼
-┠≽ *${prefix}ping*
-┃ *Desc* : Show Connection Bot Speed
-┠──────────────╼
-┠≽ *${prefix}bugreport* <text>
-┃ *Desc* : Report Bug To Owner Bot
-╿
+╔ *🔥 ${botName} 🔥*
+║
+➥ *〈 BOT INFO 〉*
+🐬 ║
+🐬 ║♡ *Prefijo* : 「  ${prefix}  」
+🐬 ║♡ *Creador* : ${ownerName}
+🐬 ║♡ *Version* : 0.0.2
+🐬 ║
+🔥>ԑ̮̑ঙ< *𝗡𝘆𝗮𝗻𝗕𝗼𝘁* >ԑ̮̑ঙ<🔥
+ *〈 ABOUT 〉*
+║
+║≽ *${prefix}info* (error)
+║ *Desc* : Informacion del bot
+║──────────────╼
+║≽ *${prefix}blocklist*
+║ *Desc* : Usuarios bloqueados del usuario
+║──────────────╼
+║≽ *${prefix}chatlist* (error)
+║ *Desc* : Muestra todos los chats del usuario
+║──────────────╼
+║≽ *${prefix}ping*
+║ *Desc* : Velocidad del bot
+║──────────────╼
+║≽ *${prefix}bugreport* <text>
+║ *Desc* : Report Bug To Owner Bot
 ┷┯ *〈 MUSIC 〉*
    ╽
    ┠≽ *${prefix}play* <music_name>
