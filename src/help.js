dabadbfb4479a33229a0d526f207a8f70ce2f9aa
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-03-14 16:44:16 -0600

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,54 @@
+const help = (prefix) => {
+	return `🐬 *𝗡𝘆𝗮𝗻𝗕𝗼𝘁* 🐬
+►〔 _*COMANDOS*_ 〕
+▎•҉➢ *${prefix}sticker* = _imagen/video/gif A sticker_
+▎•҉➢ *${prefix}toimg* = _Sticker a imagen_
+▎•҉➢ *${prefix}tts* = _Texto a voz_ (Ejemplo: *${prefix}tts es hola*)
+▎•҉➢ *${prefix}loli* = _Una loli XD_ 
+▎•҉➢ *${prefix}info* = _Informacion sobre el bot_
+▎•҉➢ *${prefix}meme* = _Ojala y sepas ingles:D_
+▎•҉➢ *${prefix}yt2mp3* = _Descarga musica de YouTube, nececitas link_
+▎•҉➢ *${prefix}yt2mp4* = _Descarga videos de YouTube, nececitas link_
+▎•҉➢ *${prefix}play* = _Descarga musica con solo nombre de la cancion_
+▎•҉➢ *${prefix}tiktok* = _Desdarga videos de tiktok_
+▎•҉➢ *${prefix}tiktokstalk* = _Info de un usuario de tiktok_
+▎•҉➢ *${prefix}all/all2/all3* = _Etiqueta a todos los del grupo_
+▎•҉➢ *${prefix}promote* = _Otorga admin_
+▎•҉➢ *${prefix}demote* = _Quita admin_
+▎•҉➢ *${prefix}kick* = _Elimina participantes del grupo_
+▎•҉➢ *${prefix}add* = _Añade participantes al grupo_
+▎•҉➢ *${prefix}simih 1/0 
+▎•҉➢ *${prefix}welcome 1/0
+ * • • ◕◕════♣♣════◕◕ • •*
+
+╔════════════════╗
+ *𝙊𝙏𝙍𝙊𝙎 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎*
+╚════════════════╝
+▎•҉➢ *${prefix}ocr*
+▎•҉➢ *${prefix}tp*
+▎•҉➢ *${prefix}ep*
+▎•҉➢ *${prefix}nulis*
+▎•҉➢ *${prefix}tsticker*
+▎•҉➢ *${prefix}bc*
+▎•҉➢ *${prefix}simi*
+▎•҉➢ *${prefix}clone*
+
+✏✏✏✏✏✏✏✏✏✏✏✏✏✏
+*OJO:*
+_ESTE BOT APENAS ESTA CRECIENDO, FUE CREADO POR SAMU330 HACE NO MAS DE 3 DIAS, SON POCOS COMANDOS, PERO MIENTRAS PASE EL TIEMPO SE AGREGARAN MAS:D_
+      
+
+*┏━┓╋┏┓*
+*┃┃┗┓┃┃* ❤ᵖSͤaͬmͬʸ❤
+*┃┏┓┗┛┣┓╋┏┳━━┳━┓*
+*┃┃┗┓┃┃┃╋┃┃┏┓┃┏┓┓*
+*┃┃╋┃┃┃┗━┛┃┏┓┃┃┃┃*
+*┗┛╋┗━┻━┓┏┻┛┗┻┛┗┛*
+*╋╋╋╋╋┏━┛┃*
+*╋╋╋╋╋┗━━┛*
+*┏━━┓╋╋╋┏┓*
+*┃┏┓┃╋╋┏┛┗┓*
+*┃┗┛┗┳━┻┓┏┛*
+*┃┏━┓┃┏┓┃┃*
+*┃┗━┛┃┗┛┃┗┓*
+*┗━━━┻━━┻━┛*
