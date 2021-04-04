60f517ff981411e864c9f37911861ffc77cabdce
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-03 18:15:36 -0600

    ⌛

--- database/menu/help.js
+++ database/menu/help.js
@@ -1,53 +1,42 @@
 const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
         return `
-┏ *〈 ${botName} 〉*
-╿
-┷┯ *〈 INFO 〉*
-   ╽
-   ┠≽ *Nombre* : ${pushname}
-   ┠≽ *XP* : ${reqXp}
-   ┠≽ *Dinero* : ${uangku}
-   ┠≽ *Registrado : ✔️
-   ╿
-┯┷ *〈 BOT INFO 〉*
-╽
-┠≽ *Prefijo* : 「  ${prefix}  」
-┠≽ *Creador* : ${ownerName}
-┠≽ *Version* : 4
-┠≽ *Canal* : http://bit.ly/ToxicGenerationV1
-╿
-┷┯ *〈 MENU 〉*
-   ┠≽ *${prefix}logomakermenu*
-   ┠≽ *${prefix}imagemakermenu*
-   ┠≽ *${prefix}stickermakermenu*
-   ┠≽ *${prefix}searchmenu*
-   ┠≽ *${prefix}educationmenu*
-   ┠≽ *${prefix}kerangmenu*
-   ┠≽ *${prefix}downloadermenu*
-   ┠≽ *${prefix}mememenu*
-   ┠≽ *${prefix}groupmenu*
-   ┠≽ *${prefix}soundmenu*
-   ┠≽ *${prefix}musicmenu*
-   ┠≽ *${prefix}islammenu*
-   ┠≽ *${prefix}stalkmenu*
-   ┠≽ *${prefix}wibumenu*
-   ┠≽ *${prefix}18+menu*
-   ┠≽ *${prefix}funmenu*
-   ┠≽ *${prefix}todmenu*
-   ┠≽ *${prefix}informationmenu*
-   ┠≽ *${prefix}stayonscreenmenu*
-   ┠≽ *${prefix}xpmenu*
-   ┠≽ *${prefix}limitmenu*
-   ┠≽ *${prefix}ownermenu*
-   ┠≽ *${prefix}othermenu*
-   ╿ *${ownerName}*,
-   ╰╼≽  © ${botName}
-   *〈 GRUPOS 〉*
-   ┠≽ *Mr-Pato 2: https://bit.ly/3wiL04v
-   ┠≽ *Félix bot: https://bit.ly/39tPICH
-   ┠≽ *Satan bot: https://bit.ly/39v9M7K
-   ╰╼
+
+   ◪ *Nombre* : ${pushname}
+   ◪ *XP* : ${reqXp}
+   ◪ *Dinero* : ${uangku}
+   ◪ *Registrado : ✔️
    
+
+○ *Prefijo* : 「  ${prefix}  」
+○ *Creador* : ${ownerName}
+○ *Version* : 3
+○ *Canal* : http://bit.ly/ToxicGenerationV1
+
+  *〈 MENU 〉*
+   ◉ *${prefix}logomakermenu*
+   ◉ *${prefix}imagemakermenu*
+   ◉ *${prefix}stickermakermenu*
+   ◉ *${prefix}searchmenu*
+   ◉ *${prefix}educationmenu*
+   ◉ *${prefix}kerangmenu*
+   ◉ *${prefix}downloadermenu*
+   ◉ *${prefix}mememenu*
+   ◉ *${prefix}groupmenu*
+   ◉ *${prefix}soundmenu*
+   ◉ *${prefix}musicmenu*
+   ◉ *${prefix}stalkmenu*
+   ◉ *${prefix}wibumenu*
+   ◉ *${prefix}18+menu*
+   ◉ *${prefix}funmenu*
+   ◉ *${prefix}todmenu*
+   ◉ *${prefix}informationmenu*
+   ◉ *${prefix}stayonscreenmenu*
+   ◉ *${prefix}xpmenu*
+   ◉ *${prefix}limitmenu*
+   ◉ *${prefix}ownermenu*
+   ◉ *${prefix}othermenu*
+   ◉*${ownerName}*,
+   ╰╼  © ${botName}
    `
 }
 exports.help = help
