a195ae00ab935409135e17fbd2a8fab1d41b712d
Samu330 <76408737+Samu330@users.noreply.github.com>
2021-04-12 16:18:37 -0500

    🌐

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const lolcatjs = require('lolcatjs')
-const figlet = require('figlet')
 
-lolcatjs.options.seed = Math.round(Math.random() * 1000);
+
 lolcatjs.options.colors = true;
 
-lolcatjs.fromString('[INFO] HOLA, SOY NyanBot')
+lolcatjs.fromString('[INFO] Suscribete a mi canal:3 Samu330')
 lolcatjs.fromString('[SYSTEM] NyanBot INICIANDOSE...')
-console.log('::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+
 let { spawn } = require('child_process')
 let path = require('path')
-const CFonts  = require('cfonts')
-CFonts.say('NyanBot', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-CFonts.say('\'Version de Prueba\' By Samu330', {
-  font: 'console',
-  align: 'center',
-  gradient: ['green', 'cyan']
-})
-CFonts.say('Contactame Para nuevas actualizaciones', {
-  font: 'console',
-  align: 'left',
-  gradient: ['blue', 'red']
-})
-console.log('::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
 
 function start() {
   let args = [path.join(__dirname, 'samu.js'), ...process.argv.slice(2)]
-  CFonts.say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  })
   let p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
   })
@@ -48,5 +25,4 @@ function start() {
 }
 
 start()
-console.log('::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
 lolcatjs.fromString('Sm330')
